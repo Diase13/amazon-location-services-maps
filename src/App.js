b0b8c592b0105ebd12cc397ce60f3fe57aa28b36
Diase13 <diego.alejandro.13@hotmail.com>
2022-09-08 15:17:54 -0500

    Fix 2.0

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import useInterval from './useInterval'
 
 const mapName = "Mapa1";
 const indexName = "Prueba"
-const trackerName = "explore.tracker" 
+const trackerName = "Tracker2" 
 const deviceID = "Celular"
 
 Amplify.configure(awsconfig);
